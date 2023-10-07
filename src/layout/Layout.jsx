64ef9402abd5e46598b02700569f79b86236c681
Mehedimid <mehedihasan25276@gmail.com>
2023-10-07 17:20:04 +0600

    create the assignment 8 project and make a simple navbar

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+function Layout(props) {
+    return (
+        <div className='sizing'>
+            <Navbar></Navbar>
+        </div>
+    );
+}
+
+export default Layout;
